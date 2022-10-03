434825f5c5634e9f388d3acab0d4b2d7a5310636
Joel Cressy <joel@jtcressy.net>
2022-10-03 02:14:50 +0000

    fix dnspolicy

--- edgenodes/k3s-bootstrap/k3sup-install.sh
+++ edgenodes/k3s-bootstrap/k3sup-install.sh
@@ -1,4 +1,4 @@
-# First Master Node
+# Initial Master Node - ONLY USE WHEN BOOTSTRAPPING CLUSTER
 k3sup install \
   --host raspberrypi-4a06138.jtcressy-home.org.github.beta.tailscale.net \
   --user ubuntu \
@@ -7,9 +7,19 @@ k3sup install \
   --local-path ~/.kube/config --merge --context=rpik3s \
   --k3s-extra-args '--flannel-iface tailscale0 --flannel-backend host-gw --disable "traefik,local-storage"'
 
-# Second Master Node
+# Master nodes should connect to the previous for initial configuration
+
 k3sup join \
-  --host raspberrypi-b38e0bf.jtcressy-home.org.github.beta.tailscale.net \
+  --host raspberrypi-4a06138.jtcressy-home.org.github.beta.tailscale.net \
+  --user ubuntu \
+  --server-user ubuntu \
+  --server-host raspberrypi-b38e0bf.jtcressy-home.org.github.beta.tailscale.net \
+  --server \
+  --k3s-channel stable \
+  --k3s-extra-args '--flannel-iface tailscale0 --flannel-backend host-gw --disable "traefik,local-storage"'
+
+k3sup join \
+  --host raspberrypi-b7162e4.jtcressy-home.org.github.beta.tailscale.net \
   --user ubuntu \
   --server-user ubuntu \
   --server-host raspberrypi-4a06138.jtcressy-home.org.github.beta.tailscale.net \
@@ -17,12 +27,11 @@ k3sup join \
   --k3s-channel stable \
   --k3s-extra-args '--flannel-iface tailscale0 --flannel-backend host-gw --disable "traefik,local-storage"'
 
-# Third Master Node
 k3sup join \
-  --host raspberrypi-b7162e4.jtcressy-home.org.github.beta.tailscale.net \
+  --host raspberrypi-b38e0bf.jtcressy-home.org.github.beta.tailscale.net \
   --user ubuntu \
   --server-user ubuntu \
-  --server-host raspberrypi-4a06138.jtcressy-home.org.github.beta.tailscale.net \
+  --server-host raspberrypi-b7162e4.jtcressy-home.org.github.beta.tailscale.net \
   --server \
   --k3s-channel stable \
   --k3s-extra-args '--flannel-iface tailscale0 --flannel-backend host-gw --disable "traefik,local-storage"'
