91926aaa6f06905d11b5e71bbb38513b97c72381
Joel Cressy <me@joelcressy.dev>
2024-11-16 09:29:38 -0600

    misc updates to magefiles

--- magefiles/fio.go
+++ magefiles/fio.go
@@ -5,30 +5,43 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
+	"os"
 	"strings"
 	"time"
 
-	"github.com/dustin/go-humanize"
-	"github.com/magefile/mage/mg"
+	"io/ioutil"
+	"log/slog"
+	"path/filepath"
 
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
+	"github.com/dustin/go-humanize"
 	"github.com/kastenhq/kubestr/pkg/fio"
 	"github.com/kastenhq/kubestr/pkg/kubestr"
+	"github.com/lainio/err2"
+	"github.com/lainio/err2/try"
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+	"github.com/samber/lo"
 
 	excelize "github.com/xuri/excelize/v2"
+	corev1 "k8s.io/api/core/v1"
+	storagev1 "k8s.io/api/storage/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type Fio mg.Namespace
 
-func runfio(ctx context.Context, testName, storageClass, nodeName string) (*fio.RunFIOResult, error) {
-	cli, err := kubestr.LoadKubeCli()
-	if err != nil {
-		fmt.Println(err.Error())
-		return nil, err
-	}
+func runfio(ctx context.Context, testName, storageClass, nodeName string) (_ *fio.RunFIOResult, err error) {
+	defer err2.Handle(&err, func(err error) error {
+		slog.Error(err.Error())
+		return err
+	})
+
 	fioRunner := &fio.FIOrunner{
-		Cli: cli,
+		Cli: try.To1(kubestr.LoadKubeCli()),
 	}
 
 	return fioRunner.RunFio(ctx, &fio.RunFIOArgs{
@@ -44,23 +57,142 @@ func runfio(ctx context.Context, testName, storageClass, nodeName string) (*fio.
 }
 
 // Run a single fio storage performance test using a specific storage class and node
-func (Fio) Single(ctx context.Context, testName, storageClass, nodeName string) error {
-	result, err := runfio(ctx, testName, storageClass, nodeName)
-	if err != nil {
-		fmt.Println(err.Error())
+func (Fio) Single(ctx context.Context) (err error) {
+	defer err2.Handle(&err, func(err error) error {
+		slog.Error(err.Error())
 		return err
+	})
+
+	kcli := try.To1(kubestr.LoadKubeCli())
+	fioRunner := &fio.FIOrunner{
+		Cli: try.To1(kubestr.LoadKubeCli()),
 	}
-	fioResultJson, err := json.Marshal(result)
-	if err != nil {
-		fmt.Println(err.Error())
-		return err
+
+	var (
+		fioJobFile   os.FileInfo
+		storageClass *storagev1.StorageClass
+		node         *corev1.Node
+		// confirm      bool
+	)
+	// get git dir root path
+	rootPath := try.To1(sh.OutCmd("git")("rev-parse", "--show-toplevel"))
+	// todo: interactive prompt for test, storageclass, and node
+	try.To(huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[os.FileInfo]().
+				Title("Fio Job").
+				OptionsFunc(func() (options []huh.Option[os.FileInfo]) {
+					files := try.To1(ioutil.ReadDir(filepath.Join(rootPath, "magefiles/fio-tests/")))
+					for _, file := range lo.Filter(files, func(fileInfo os.FileInfo, _ int) bool {
+						return strings.HasSuffix(fileInfo.Name(), ".fio")
+					}) {
+						if !file.IsDir() {
+							options = append(options, huh.Option[os.FileInfo]{
+								Key:   strings.TrimSuffix(file.Name(), ".fio"),
+								Value: file,
+							})
+						}
+					}
+					return options
+				}, &kcli).
+				Value(&fioJobFile).WithHeight(6),
+			// ), huh.NewGroup(
+			huh.NewSelect[*storagev1.StorageClass]().
+				Title("Storage Class").
+				OptionsFunc(func() (options []huh.Option[*storagev1.StorageClass]) {
+					scList := try.To1(kcli.StorageV1().StorageClasses().List(ctx, metav1.ListOptions{}))
+
+					for _, sc := range scList.Items {
+						options = append(options, huh.Option[*storagev1.StorageClass]{
+							Key:   sc.ObjectMeta.Name,
+							Value: &sc,
+						})
+					}
+					return options
+				}, &fioJobFile).
+				Value(&storageClass).WithHeight(6),
+			// ), huh.NewGroup(
+			huh.NewSelect[*corev1.Node]().
+				Title("Target Node").
+				OptionsFunc(func() (options []huh.Option[*corev1.Node]) {
+					nodeList := try.To1(kcli.CoreV1().Nodes().List(ctx, metav1.ListOptions{}))
+
+					for _, node := range nodeList.Items {
+						options = append(options, huh.Option[*corev1.Node]{
+							Key:   node.ObjectMeta.Name,
+							Value: &node,
+						})
+					}
+					return options
+				}, &storageClass).
+				Value(&node).WithHeight(6),
+		// ), huh.NewGroup(
+		// 	huh.NewConfirm().
+		// 		TitleFunc(func() string {
+		// 			return fmt.Sprintf(
+		// 				"Run %s test using %s storage class on node %s?",
+		// 				strings.TrimSuffix(fioJobFile.Name(), ".fio"),
+		// 				storageClass.ObjectMeta.Name,
+		// 				node.ObjectMeta.Name,
+		// 			)
+		// 		}, &node).Value(&confirm),
+		),
+	).WithTheme(huh.ThemeCharm()).RunWithContext(ctx))
+
+	// if !confirm {
+	// 	slog.Warn("user aborted")
+	// 	return nil
+	// }
+
+	result := try.To1(fioRunner.RunFio(ctx, &fio.RunFIOArgs{
+		StorageClass: storageClass.ObjectMeta.Name,
+		Size:         "20Gi",
+		Namespace:    "fio",
+		NodeSelector: map[string]string{
+			"kubernetes.io/hostname": node.ObjectMeta.Name,
+		},
+		FIOJobName:     fmt.Sprintf("%s-%s-%s", strings.TrimSuffix(fioJobFile.Name(), ".fio"), storageClass.ObjectMeta.Name, node.ObjectMeta.Name),
+		FIOJobFilepath: filepath.Join(rootPath, "magefiles/fio-tests", fioJobFile.Name()),
+	}))
+
+	if len(result.Result.Jobs) == 0 {
+		slog.Debug("no results found", slog.Any("result", result))
+		return fmt.Errorf("no results found")
 	}
-	fmt.Println(string(fioResultJson))
+	t := table.New().
+		Border(lipgloss.NormalBorder()).
+		BorderStyle(lipgloss.NewStyle().Foreground(lipgloss.Color("99"))).
+		StyleFunc(func(row, col int) lipgloss.Style {
+			switch {
+			case row == 0:
+				return lipgloss.NewStyle().Foreground(lipgloss.Color("99")).Bold(true).Align(lipgloss.Center)
+			default:
+				return lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
+			}
+		}).
+		Headers("Storage Class", "Test Name", "Size", "Read IOPS", "Read Bandwidth", "Write IOPS", "Write Bandwidth", "Avg. Read Latency", "Avg. Write Latency", "Disk Util %").
+		Rows([]string{
+			result.StorageClass.ObjectMeta.Name,
+			result.Result.Jobs[0].JobName,
+			result.Size,
+			fmt.Sprintf("%0.f", result.Result.Jobs[0].Read.Iops),
+			fmt.Sprintf("%s/s", humanize.Bytes(uint64(result.Result.Jobs[0].Write.BWBytes))),
+			fmt.Sprintf("%dµs", time.Duration(result.Result.Jobs[0].Read.LatNs.Mean*float32(time.Nanosecond)).Microseconds()),
+			fmt.Sprintf("%dµs", time.Duration(result.Result.Jobs[0].Write.LatNs.Mean*float32(time.Nanosecond)).Microseconds()),
+			fmt.Sprintf("%0.f", lo.If(len(result.Result.DiskUtil) > 0, result.Result.DiskUtil[len(result.Result.DiskUtil)-1].Util).Else(0)),
+		})
+	fmt.Println(t)
 	return nil
 }
 
 // Run a predefined suite of fio storage tests for one or more storageClasses on a node and output results to an excel worksheet
-func (Fio) Suite(ctx context.Context, nodeName string, storageClasses string) error {
+func (Fio) Suite(ctx context.Context, nodeName string, storageClasses string) (err error) {
+	defer err2.Handle(&err, func(err error) error {
+		slog.Error(err.Error())
+		return err
+	})
+	// todo: interactive prompt for node, with multiselect for test names and storageclasses
+
 	var results []*fio.RunFIOResult
 	for _, storageClass := range strings.Split(storageClasses, ",") {
 		for _, testName := range []string{"fio-seq-read", "fio-seq-write", "fio-rand-read", "fio-rand-write"} {
@@ -73,14 +205,13 @@ func (Fio) Suite(ctx context.Context, nodeName string, storageClasses string) er
 			results = append(results, result)
 		}
 	}
-	fileName := fmt.Sprintf("%s-fio-suite-results.xlsx", nodeName)
-	fmt.Println(fmt.Sprintf("Generating excel spreadsheet with results... (%s)", fileName))
-	_, err := generateExcelSpreadsheet(results, fileName)
-	if err != nil {
-		fmt.Println(err.Error())
-		return err
-	}
-	fmt.Println("Done!")
+
+	// todo: print results in a table
+	// todo: interactively ask if user wants to save results to an excel file
+	fileName := fmt.Sprintf("%s_%s_fio-suite-results.xlsx", nodeName, strings.ReplaceAll(storageClasses, ",", "_"))
+	slog.Info("Generating excel spreadsheet with results...", slog.String("fileName", fileName))
+	_ = try.To1(generateExcelSpreadsheet(results, fileName))
+	slog.Info("Done!", slog.String("fileName", fileName))
 	return nil
 }
 
--- magefiles/go.mod
+++ magefiles/go.mod
@@ -5,32 +5,52 @@ go 1.23.1
 toolchain go1.23.3
 
 require (
+	github.com/charmbracelet/huh v0.6.0
+	github.com/charmbracelet/lipgloss v0.13.0
+	github.com/charmbracelet/log v0.4.0
 	github.com/dustin/go-humanize v1.0.1
+	github.com/go-logr/logr v1.4.2
 	github.com/kastenhq/kubestr v0.4.46
+	github.com/lainio/err2 v1.0.0
 	github.com/magefile/mage v1.15.0
+	github.com/samber/lo v1.47.0
 	github.com/xuri/excelize/v2 v2.9.0
+	k8s.io/api v0.30.3
+	k8s.io/apimachinery v0.30.3
+	k8s.io/klog/v2 v2.130.1
 	tailscale.com v1.76.6
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.13.0 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/MakeNowJust/heredoc v1.0.0 // indirect
 	github.com/Masterminds/semver v1.5.0 // indirect
-	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/akutz/memconn v0.1.0 // indirect
 	github.com/alexbrainman/sspi v0.0.0-20231016080023-1a75b4708caa // indirect
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/briandowns/spinner v1.23.1 // indirect
+	github.com/catppuccin/go v0.2.0 // indirect
 	github.com/chai2010/gettext-go v1.0.2 // indirect
+	github.com/charmbracelet/bubbles v0.20.0 // indirect
+	github.com/charmbracelet/bubbletea v1.1.0 // indirect
+	github.com/charmbracelet/x/ansi v0.2.3 // indirect
+	github.com/charmbracelet/x/exp/strings v0.0.0-20240722160745-212f7b056ed0 // indirect
+	github.com/charmbracelet/x/term v0.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/dblohm7/wingoes v0.0.0-20240119213807-a09d6be7affa // indirect
 	github.com/emicklei/go-restful/v3 v3.11.2 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
 	github.com/exponent-io/jsonpath v0.0.0-20151013193312-d6023ce2651d // indirect
 	github.com/fatih/color v1.17.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.6.0 // indirect
 	github.com/go-errors/errors v1.4.2 // indirect
-	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-json-experiment/json v0.0.0-20231102232822-2e55bd4e08b0 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/go-openapi/errors v0.22.0 // indirect
 	github.com/go-openapi/jsonpointer v0.20.2 // indirect
 	github.com/go-openapi/jsonreference v0.20.4 // indirect
@@ -40,11 +60,12 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/btree v1.1.2 // indirect
-	github.com/google/gnostic v0.6.9 // indirect
+	github.com/google/gnostic-models v0.6.9-0.20230804172637-c7be7c783f49 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/gregjones/httpcache v0.0.0-20180305231024-9cad4c3443a7 // indirect
 	github.com/hdevalence/ed25519consensus v0.2.0 // indirect
 	github.com/imdario/mergo v0.3.16 // indirect
@@ -54,18 +75,23 @@ require (
 	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/jsimonetti/rtnetlink v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kanisterio/errkit v0.0.2 // indirect
 	github.com/kanisterio/kanister v0.0.0-20240805080534-ea6cb88542d6 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/kubernetes-csi/external-snapshotter/client/v4 v4.2.0 // indirect
 	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mdlayher/netlink v1.7.2 // indirect
 	github.com/mdlayher/socket v0.5.0 // indirect
 	github.com/mitchellh/go-ps v1.0.0 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/moby/spdystream v0.2.0 // indirect
 	github.com/moby/term v0.0.0-20221205130635-1aeaba878587 // indirect
@@ -73,7 +99,11 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/termenv v0.15.3-0.20240618155329-98d742f6907a // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/mxk/go-flowrate v0.0.0-20140419014527-cca7078d478f // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/openshift/api v0.0.0-20231222123017-053aee22b4b4 // indirect
 	github.com/openshift/client-go v0.0.0-20231221125933-2aa81c72f992 // indirect
@@ -81,11 +111,13 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.4 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tailscale/go-winio v0.0.0-20231025203758-c4f33415bf55 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	github.com/xlab/treeprint v1.2.0 // indirect
 	github.com/xuri/efp v0.0.0-20240408161823-9ad904a10d6d // indirect
@@ -106,21 +138,17 @@ require (
 	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.23.0 // indirect
 	golang.zx2c4.com/wireguard/windows v0.5.3 // indirect
-	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.30.3 // indirect
 	k8s.io/apiextensions-apiserver v0.30.3 // indirect
-	k8s.io/apimachinery v0.30.3 // indirect
 	k8s.io/cli-runtime v0.29.7 // indirect
 	k8s.io/client-go v0.30.3 // indirect
 	k8s.io/code-generator v0.30.3 // indirect
 	k8s.io/component-base v0.30.3 // indirect
-	k8s.io/gengo v0.0.0-20230829151522-9cce18d56c01 // indirect
-	k8s.io/klog/v2 v2.130.1 // indirect
+	k8s.io/gengo/v2 v2.0.0-20240228010128-51d4e06bde70 // indirect
 	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
 	k8s.io/kubectl v0.29.7 // indirect
 	k8s.io/utils v0.0.0-20240711033017-18e509b52bc8 // indirect
