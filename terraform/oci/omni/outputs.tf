5e2d15e1a48bff4ae5279ba74ae0723a16cd6e09
Joel Cressy <me@joelcressy.dev>
2025-12-02 12:26:00 -0600

    fix(terraform): comment out outputs until VM resources exist
    Outputs reference oci_core_instance.omni_vm which will be created in
    issues #529-#531. Commenting out to allow terraform validate to pass
    for issue #528 which only creates directory structure and backend config.
    
    Outputs will be uncommented when VM resources are added.

--- terraform/oci/omni/outputs.tf
+++ terraform/oci/omni/outputs.tf
@@ -1,19 +1,22 @@
-output "vm_public_ip" {
-  description = "Public IP address of the VM"
-  value       = oci_core_instance.omni_vm.public_ip
-}
+# Outputs commented out until VM resources are created in issues #529-#531
+# Uncomment when oci_core_instance.omni_vm resource is added
 
-output "vm_private_ip" {
-  description = "Private IP address of the VM"
-  value       = oci_core_instance.omni_vm.private_ip
-}
+# output "vm_public_ip" {
+#   description = "Public IP address of the VM"
+#   value       = oci_core_instance.omni_vm.public_ip
+# }
 
-output "instance_id" {
-  description = "OCI instance OCID"
-  value       = oci_core_instance.omni_vm.id
-}
+# output "vm_private_ip" {
+#   description = "Private IP address of the VM"
+#   value       = oci_core_instance.omni_vm.private_ip
+# }
 
-output "instance_ocid" {
-  description = "Instance OCID for reference"
-  value       = oci_core_instance.omni_vm.id
-}
+# output "instance_id" {
+#   description = "OCI instance OCID"
+#   value       = oci_core_instance.omni_vm.id
+# }
+
+# output "instance_ocid" {
+#   description = "Instance OCID for reference"
+#   value       = oci_core_instance.omni_vm.id
+# }
